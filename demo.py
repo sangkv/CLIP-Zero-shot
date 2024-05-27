74761a8bfd157fc3c814768b4b8e03099e580f41
sangkv <sangkv.work@gmail.com>
2024-05-27 21:10:52 +0700

    add time

--- demo.py
+++ demo.py
@@ -1,4 +1,5 @@
 import os
+import time
 from PIL import Image
 from models import ImageClassifier
 
@@ -7,9 +8,15 @@ categories = ['a man', 'a woman', 'flowers', 'a house', 'a little girl', 'A floc
 # Initialize the classifier with the above category
 classifier = ImageClassifier(categories=categories)
 
-for file in os.listdir('data/'):
+listFiles = os.listdir('data/')
+sumTime = 0
+for file in listFiles:
     image = Image.open('data/'+file)
+
+    t0 = time.time()
     result, probability = classifier.predict(image)
+    t1 = time.time()
+    sumTime = sumTime + (t1-t0)
 
-    print("\nThe predicted result of image '%s' is: "%file, result, " ", probability)
-
+    print("\nThe predicted result of image '%s' is: "%file, result, ", Probability: ", probability)
+print("\n\nSum time: ", sumTime, ", The average time: ", (sumTime/len(listFiles)))
